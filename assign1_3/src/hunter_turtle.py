99c6b1b4edf99a63ff546a7b701813775bdadc1f
zainasir <znasir1@binghamton.edu>
2023-02-15 23:12:39 -0500

    added spawn turtle and pub sub for hunter

--- assign1_3/src/hunter_turtle.py
+++ assign1_3/src/hunter_turtle.py
@@ -4,13 +4,38 @@ import rospy
 import time
 import math
 from turtlesim.srv import *
+from geometry_msgs.msg import Twist
+from std_msgs.msg import Bool
+from turtlesim.msg import Pose
 
 class Hunter:
     def __init__(self):
         rospy.wait_for_service('/spawn')
         spawn_hunter = rospy.ServiceProxy('/spawn', Spawn)
         spawn_hunter(1, 1, 0, "hunter_turtle")
-        print("spawned the turtle")
+
+        self.linear_velocity = 2
+        self.velocity_pub = rospy.Publisher('/hunter_turtle/cmd_vel', Twist, queue_size=10)
+        self.kill_pub = rospy.Publisher('/hunter_turtle/kill_runner', Bool, queue_size=1)
+        self.vel_msg = Twist()
+        self.vel_msg.linear.x = self.linear_velocity
+
+        self.hunter_pos_subscriber = rospy.Subscriber('/hunter_turtle/pose', Pose, self.update_hunter_pose)
+        self.runner_pose_subscriber = rospy.Subscriber('/runner_turtle/pose', Pose, self.update_runner_pose)
+
+        self.hunter_pose = Pose(1, 1, 0, 0, 0)
+        self.runner_pose = Pose(1, 1, 0, 0, 0)
+
+        self.max_ang_vel = 2
+        
+    def start_hunt(self):
+        pass
+
+    def update_hunter_pose(self, pos):
+        self.hunter_pose = pos
+
+    def update_runner_pose(self, pos):
+        self.runner_pose = pos
         
 if __name__ == "__main__":
     rospy.init_node('hunt')
